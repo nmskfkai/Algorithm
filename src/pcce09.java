933beafe8b8f851e8057f32a92521070965ddc31
nmskfkai <leejimin00@naver.com>
2024-11-06 17:04:12 +0900

    20241106 백준 2750

---
+++ src/pcce09.java
@@ -0,0 +1,8 @@
+public class pcce09 {
+    static class Solution {
+        public int solution(int[] wallet, int[] bill) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
