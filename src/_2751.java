0a39984c4397d7e2c9e349e1d012972a5e2a7922
nmskfkai <leejimin00@naver.com>
2024-11-08 20:57:29 +0900

    20241108 백준 2751 시간복잡도 해결 "BufferedReader, Collections.sort 등"

--- src/_2751.java
+++ src/_2751.java
@@ -22,24 +22,44 @@ N개의 수가 주어졌을 때, 오름차순으로 정렬
 
  */
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class _2751 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        /*
+        버퍼 ? 데이터를 한 곳에서 다른 곳으로 전송하는 동안 일시적으로 데이터를 보관하는 임시 메모리 영역
+        입출력 속도 향상
+        BufferedReader 는 String 만 입력 받을 수 있다.
+        int 값 할당받고 싶으면 int num = Integer.parseInt(br.readLine()); 처럼 형변환 필요
+         */
 
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int [] arr = new int [N];
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for(int i = 0; i < N; i++){
+            list.add(Integer.parseInt(br.readLine()));
         }
 
-        Arrays.sort(arr);
-        for (int i = 0; i < N; i++) {
-            System.out.println(arr[i]);
+        br.close();
+
+
+        // Arrays.sort 는 시간복잡도 O(nlogn),
+        // Timsort 를 사용하는데, 이는 삽입 정렬과 병합정렬을 결합한 알고리즘
+        Collections.sort(list);
+
+        for(int i : list){
+            bw.write(i + "\n");
         }
+
+        bw.flush();
+        bw.close();
+
+
     }
 }
